9e2fa4407d8859cb387d910125297e46e410e0c8
Paketgit <maxim.ipati2006@gmail.com>
2025-07-11 02:15:05 +0300

    Gate variations
    4 variaton

--- Assets/Scripts/GateController.cs
+++ Assets/Scripts/GateController.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
 public class GateController : MonoBehaviour
 {
@@ -7,9 +7,21 @@ public class GateController : MonoBehaviour
     [Space]
 
     [SerializeField] public int gateValue;
+
+    [Space]
+    [Header("Operatinos")]
+    [SerializeField] public bool addition;
+    [SerializeField] public bool subtraction;
+    [SerializeField] public bool multiplication;
+    [SerializeField] public bool division;
+
     void Start()
     {
-        textGateValue.text = "+" + gateValue.ToString();
+        if (addition) textGateValue.text = "+" + gateValue.ToString();
+        else if (subtraction) textGateValue.text = "-" + gateValue.ToString();
+        else if (multiplication) textGateValue.text = "*" + gateValue.ToString();
+        else if (division) textGateValue.text = "÷" + gateValue.ToString();
+
     }
 
     void Update()
@@ -22,8 +34,16 @@ public class GateController : MonoBehaviour
         if (other.name == "Player")
         {
             PlayerController playerData = other.GetComponent<PlayerController>();
+
             if (playerData == null) { return; }
-            playerData.value = playerData.value += gateValue;
+            
+            if (addition) { playerData.value = playerData.value += gateValue; }
+            else if (subtraction) { playerData.value = playerData.value -= gateValue; }
+            else if (multiplication) { playerData.value = playerData.value *= gateValue; }
+            else if (division) { playerData.value = playerData.value /= gateValue; }
+
+            if (playerData.value < 0) { playerData.value = 0; }
+            
             Debug.Log(playerData.value);
         }
     }
