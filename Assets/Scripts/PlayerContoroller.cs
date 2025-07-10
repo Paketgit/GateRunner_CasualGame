547917f1919d52462851edacdcb970c7fafa60ae
Paketgit <maxim.ipati2006@gmail.com>
2025-07-10 19:32:15 +0300

    semi_controller

--- Assets/Scripts/PlayerContoroller.cs
+++ Assets/Scripts/PlayerContoroller.cs
@@ -2,19 +2,34 @@ using UnityEngine;
 
 public class PlayerContoroller : MonoBehaviour
 {
-    //CharacterController contorller; // <- дл€ физики, если прыжки ху€рить будем
-    Transform axis;
+    CharacterController controller; // <- дл€ физики, если прыжки ху€рить будем
 
     [SerializeField] public int value;
+    [Space]
+    [SerializeField] private Vector3 playerVelocity;
+    [SerializeField] private bool groundedPlayer;
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = -9.81f;
     void Start()
     {
         value = 0;
-        axis = GetComponent<Transform>();
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        axis.position -= Vector3.forward;
+        Move();
+        
+    }
+
+    private void Move()
+    {
+        controller.Move(transform.TransformDirection(Vector3.forward) * speed);
+    }
+
+    private void Jump()
+    {
+        controller.velocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
     }
 }
